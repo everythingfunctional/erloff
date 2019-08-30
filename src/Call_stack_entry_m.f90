48dd7bdda26a6eeede6bfcd8d2c7d104ac3e5cdb
Brad Richardson <brichardson@structint.com>
2019-08-30 11:31:10 -0700

    Implement a call stack entry type

---
+++ src/Call_stack_entry_m.f90
@@ -0,0 +1,125 @@
+module Call_stack_entry_m
+    use iso_varying_string, only: VARYING_STRING
+
+    implicit none
+    private
+
+    type, public :: CallStackEntry_t
+        private
+        type(VARYING_STRING) :: module_name
+        type(VARYING_STRING) :: procedure_name
+    contains
+        private
+        procedure, public :: toString
+        procedure :: isFromModuleC
+        procedure :: isFromModuleS
+        generic, public :: operator(.isFromModule.) => &
+                isFromModuleC, isFromModuleS
+        procedure :: isFromProcedureC
+        procedure :: isFromProcedureS
+        generic, public :: operator(.isFromProcedure.) => &
+                isFromProcedureC, isFromProcedureS
+    end type CallStackEntry_t
+
+    interface CallStackEntry
+        module procedure CallStackEntryCC
+        module procedure CallStackEntryCS
+        module procedure CallStackEntrySC
+        module procedure CallStackEntrySS
+    end interface CallStackEntry
+
+    public :: CallStackEntry
+contains
+    pure function CallStackEntryCC(module_name, procedure_name) result(entry_)
+        use iso_varying_string, only: assignment(=)
+
+        character(len=*), intent(in) :: module_name
+        character(len=*), intent(in) :: procedure_name
+        type(CallStackEntry_t) :: entry_
+
+        entry_%module_name = module_name
+        entry_%procedure_name = procedure_name
+    end function CallStackEntryCC
+
+    pure function CallStackEntryCS(module_name, procedure_name) result(entry_)
+        use iso_varying_string, only: VARYING_STRING, assignment(=)
+
+        character(len=*), intent(in) :: module_name
+        type(VARYING_STRING), intent(in) :: procedure_name
+        type(CallStackEntry_t) :: entry_
+
+        entry_%module_name = module_name
+        entry_%procedure_name = procedure_name
+    end function CallStackEntryCS
+
+    pure function CallStackEntrySC(module_name, procedure_name) result(entry_)
+        use iso_varying_string, only: VARYING_STRING, assignment(=)
+
+        type(VARYING_STRING), intent(in) :: module_name
+        character(len=*), intent(in) :: procedure_name
+        type(CallStackEntry_t) :: entry_
+
+        entry_%module_name = module_name
+        entry_%procedure_name = procedure_name
+    end function CallStackEntrySC
+
+    pure function CallStackEntrySS(module_name, procedure_name) result(entry_)
+        use iso_varying_string, only: VARYING_STRING
+
+        type(VARYING_STRING), intent(in) :: module_name
+        type(VARYING_STRING), intent(in) :: procedure_name
+        type(CallStackEntry_t) :: entry_
+
+        entry_%module_name = module_name
+        entry_%procedure_name = procedure_name
+    end function CallStackEntrySS
+
+    pure function toString(self) result(string)
+        use iso_varying_string, only: VARYING_STRING, operator(//)
+
+        class(CallStackEntry_t), intent(in) :: self
+        type(VARYING_STRING) :: string
+
+        string = self%module_name // "." // self%procedure_name
+    end function toString
+
+    elemental function isFromModuleC(self, module_name)
+        use iso_varying_string, only: operator(==)
+
+        class(CallStackEntry_t), intent(in) :: self
+        character(len=*), intent(in) :: module_name
+        logical :: isFromModuleC
+
+        isFromModuleC = self%module_name == module_name
+    end function isFromModuleC
+
+    elemental function isFromModuleS(self, module_name)
+        use iso_varying_string, only: VARYING_STRING, operator(==)
+
+        class(CallStackEntry_t), intent(in) :: self
+        type(VARYING_STRING), intent(in) :: module_name
+        logical :: isFromModuleS
+
+        isFromModuleS = self%module_name == module_name
+    end function isFromModuleS
+
+    elemental function isFromProcedureC(self, procedure_name)
+        use iso_varying_string, only: operator(==)
+
+        class(CallStackEntry_t), intent(in) :: self
+        character(len=*), intent(in) :: procedure_name
+        logical :: isFromProcedureC
+
+        isFromProcedureC = self%procedure_name == procedure_name
+    end function isFromProcedureC
+
+    elemental function isFromProcedureS(self, procedure_name)
+        use iso_varying_string, only: VARYING_STRING, operator(==)
+
+        class(CallStackEntry_t), intent(in) :: self
+        type(VARYING_STRING), intent(in) :: procedure_name
+        logical :: isFromProcedureS
+
+        isFromProcedureS = self%procedure_name == procedure_name
+    end function isFromProcedureS
+end module Call_stack_entry_m
---
+++ tests/call_stack_entry_test.f90
@@ -0,0 +1,60 @@
+module call_stack_entry_test
+    implicit none
+    private
+
+    public :: test_call_stack_entry
+contains
+    function test_call_stack_entry() result(tests)
+        use Vegetables_m, only: TestItem_t, describe, it
+
+        type(TestItem_t) :: tests
+
+        type(TestItem_t) :: individual_tests(2)
+
+        individual_tests(1) = it( &
+                "Can tell if it is from a module", checkIsFromModule)
+        individual_tests(2) = it( &
+                "Can tell if it is from a procedure", checkIsFromProcedure)
+        tests = describe("CallStackEntry_t", individual_tests)
+    end function test_call_stack_entry
+
+    pure function checkIsFromModule() result(result_)
+        use Call_stack_entry_m, only: CallStackEntry_t, CallStackEntry
+        use iso_varying_string, only: operator(//)
+        use Vegetables_m, only: Result_t, assertNot, assertThat
+
+        type(Result_t) :: result_
+
+        type(CallStackEntry_t) :: entry_
+
+        entry_ = CallStackEntry("Some_module_m", "someProcedure")
+
+        result_ = &
+                assertThat( &
+                        entry_.isFromModule."Some_module_m", &
+                        entry_%toString() // '.isFromModule."Some_module_m"') &
+                .and.assertNot( &
+                        entry_.isFromModule."Other_module_m", &
+                        entry_%toString() // '.isFromModule."Other_module_m"')
+    end function checkIsFromModule
+
+    pure function checkIsFromProcedure() result(result_)
+        use Call_stack_entry_m, only: CallStackEntry_t, CallStackEntry
+        use iso_varying_string, only: operator(//)
+        use Vegetables_m, only: Result_t, assertNot, assertThat
+
+        type(Result_t) :: result_
+
+        type(CallStackEntry_t) :: entry_
+
+        entry_ = CallStackEntry("Some_module_m", "someProcedure")
+
+        result_ = &
+                assertThat( &
+                        entry_.isFromProcedure."someProcedure", &
+                        entry_%toString() // '.isFromProcedure."someProcedure"') &
+                .and.assertNot( &
+                        entry_.isFromProcedure."otherProcedure", &
+                        entry_%toString() // '.isFromProcedure."otherProcedure"')
+    end function checkIsFromProcedure
+end module call_stack_entry_test
