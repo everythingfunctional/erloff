006bd04537a51e1bb44b725aac7b2ae1ea41f9db
Brad Richardson <brichardson@structint.com>
2019-09-18 09:19:12 -0700

    Change CallStack to use the new types

--- src/Call_stack_m.f90
+++ src/Call_stack_m.f90
@@ -4,322 +4,180 @@ module Call_stack_m
     implicit none
     private
 
+    type :: Node_t
+        type(CallStackEntry_t) :: entry_
+        type(Node_t), pointer :: next
+    end type Node_t
+
     type, public :: CallStack_t
         private
-        type(CallStackEntry_t), allocatable :: entries(:)
+        integer :: depth = 0
+        type(Node_t), pointer :: head => null()
+        type(Node_t), pointer :: tail => null()
     contains
         private
-        procedure :: prependNamesCC
-        procedure :: prependNamesCS
-        procedure :: prependNamesSC
-        procedure :: prependNamesSS
-        generic, public :: prependNames => &
-                prependNamesCC, prependNamesCS, prependNamesSC, prependNamesSS
+        procedure, public :: prependNames
         procedure, public :: toString
-        procedure :: originatingModuleIsC
-        procedure :: originatingModuleIsS
-        generic, public :: operator(.originatingModuleIs.) => &
-                originatingModuleIsC, originatingModuleIsS
-        procedure :: originatingProcedureIsC
-        procedure :: originatingProcedureIsS
-        generic, public :: operator(.originatingProcedureIs.) => &
-                originatingProcedureIsC, originatingProcedureIsS
-        procedure :: containsModuleC
-        procedure :: containsModuleS
-        generic, public :: operator(.containsModule.) => &
-                containsModuleC, containsModuleS
-        procedure :: containsProcedureC
-        procedure :: containsProcedureS
-        generic, public :: operator(.containsProcedure.) => &
-                containsProcedureC, containsProcedureS
+        procedure :: originatedFromModule
+        procedure :: originatedFromProcedure
+        generic, public :: operator(.originatedFrom.) => &
+                originatedFromModule, originatedFromProcedure
+        procedure :: includesModule
+        procedure :: includesProcedure
+        generic, public :: operator(.includes.) => &
+                includesModule, includesProcedure
         procedure, public :: repr
+        final :: destructor
     end type CallStack_t
-
-    interface CallStack
-        module procedure CallStackCC
-        module procedure CallStackCS
-        module procedure CallStackSC
-        module procedure CallStackSS
-    end interface CallStack
 
     public :: CallStack
 contains
-    pure function CallStackCC(module_name, procedure_name) result(new_stack)
+    pure function CallStack(module_, procedure_)
         use Call_stack_entry_m, only: CallStackEntry
+        use Module_m, only: Module_t
+        use Procedure_m, only: Procedure_t
 
-        character(len=*), intent(in) :: module_name
-        character(len=*), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
+        type(Module_t), intent(in) :: module_
+        type(Procedure_t), intent(in) :: procedure_
+        type(CallStack_t) :: CallStack
 
-        allocate(new_stack%entries(1))
-        new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-    end function CallStackCC
+        allocate(CallStack%head)
+        CallStack%head%entry_ = CallStackEntry(module_, procedure_)
+        nullify(CallStack%head%next)
+        CallStack%tail => CallStack%head
+        CallStack%depth = 1
+    end function CallStack
 
-    pure function CallStackCS(module_name, procedure_name) result(new_stack)
+    subroutine prependNames(self, module_, procedure_)
         use Call_stack_entry_m, only: CallStackEntry
+        use Module_m, only: Module_t
+        use Procedure_m, only: Procedure_t
+
+        class(CallStack_t), intent(inout) :: self
+        type(Module_t), intent(in) :: module_
+        type(Procedure_t), intent(in) :: procedure_
+
+        type(Node_t), pointer :: new_head
+
+        allocate(new_head)
+        new_head%entry_ = CallStackEntry(module_, procedure_)
+        new_head%next => self%head
+        self%head => new_head
+        self%depth = self%depth + 1
+    end subroutine prependNames
+
+    function toString(self) result(string)
         use iso_varying_string, only: VARYING_STRING
-
-        character(len=*), intent(in) :: module_name
-        type(VARYING_STRING), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        allocate(new_stack%entries(1))
-        new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-    end function CallStackCS
-
-    pure function CallStackSC(module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-        use iso_varying_string, only: VARYING_STRING
-
-        type(VARYING_STRING), intent(in) :: module_name
-        character(len=*), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        allocate(new_stack%entries(1))
-        new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-    end function CallStackSC
-
-    pure function CallStackSS(module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-        use iso_varying_string, only: VARYING_STRING
-
-        type(VARYING_STRING), intent(in) :: module_name
-        type(VARYING_STRING), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        allocate(new_stack%entries(1))
-        new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-    end function CallStackSS
-
-    pure function prependNamesCC( &
-            self, module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: module_name
-        character(len=*), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        integer :: new_stack_length
-
-        if (allocated(self%entries)) then
-            new_stack_length = size(self%entries) + 1
-            allocate(new_stack%entries(new_stack_length))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-            new_stack%entries(2:new_stack_length) = self%entries(:)
-        else
-            allocate(new_stack%entries(1))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-        end if
-    end function prependNamesCC
-
-    pure function prependNamesCS( &
-            self, module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-        use iso_varying_string, only: VARYING_STRING
-
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: module_name
-        type(VARYING_STRING), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        integer :: new_stack_length
-
-        if (allocated(self%entries)) then
-            new_stack_length = size(self%entries) + 1
-            allocate(new_stack%entries(new_stack_length))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-            new_stack%entries(2:new_stack_length) = self%entries(:)
-        else
-            allocate(new_stack%entries(1))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-        end if
-    end function prependNamesCS
-
-    pure function prependNamesSC( &
-            self, module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-        use iso_varying_string, only: VARYING_STRING
-
-        class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: module_name
-        character(len=*), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        integer :: new_stack_length
-
-        if (allocated(self%entries)) then
-            new_stack_length = size(self%entries) + 1
-            allocate(new_stack%entries(new_stack_length))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-            new_stack%entries(2:new_stack_length) = self%entries(:)
-        else
-            allocate(new_stack%entries(1))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-        end if
-    end function prependNamesSC
-
-    pure function prependNamesSS( &
-            self, module_name, procedure_name) result(new_stack)
-        use Call_stack_entry_m, only: CallStackEntry
-        use iso_varying_string, only: VARYING_STRING
-
-        class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: module_name
-        type(VARYING_STRING), intent(in) :: procedure_name
-        type(CallStack_t) :: new_stack
-
-        integer :: new_stack_length
-
-        if (allocated(self%entries)) then
-            new_stack_length = size(self%entries) + 1
-            allocate(new_stack%entries(new_stack_length))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-            new_stack%entries(2:new_stack_length) = self%entries(:)
-        else
-            allocate(new_stack%entries(1))
-            new_stack%entries(1) = CallStackEntry(module_name, procedure_name)
-        end if
-    end function prependNamesSS
-
-    pure function toString(self) result(string)
-        use iso_varying_string, only: VARYING_STRING, assignment(=)
         use strff, only: join
 
         class(CallStack_t), intent(in) :: self
         type(VARYING_STRING) :: string
 
-        if (allocated(self%entries)) then
-            string = join(self%entries%toString(), "->")
-        else
-            string = ""
-        end if
+        type(Node_t), pointer :: current
+        type(VARYING_STRING) :: entry_strings(self%depth)
+        integer :: i
+
+        current => self%head
+        do i = 1, self%depth
+            entry_strings(i) = current%entry_%toString()
+            current => current%next
+        end do
+        string = join(entry_strings, "->")
     end function toString
 
-    pure function originatingModuleIsC(self, module_name)
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: module_name
-        logical :: originatingModuleIsC
-
-        if (allocated(self%entries)) then
-            originatingModuleIsC = &
-                    self%entries(size(self%entries)).isFromModule.module_name
-        else
-            originatingModuleIsC = .false.
-        end if
-    end function originatingModuleIsC
-
-    pure function originatingModuleIsS(self, module_name)
-        use iso_varying_string, only: VARYING_STRING
+    pure function originatedFromModule(self, module_)
+        use Module_m, only: Module_t
 
         class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: module_name
-        logical :: originatingModuleIsS
+        type(Module_t), intent(in) :: module_
+        logical :: originatedFromModule
 
-        if (allocated(self%entries)) then
-            originatingModuleIsS = &
-                    self%entries(size(self%entries)).isFromModule.module_name
-        else
-            originatingModuleIsS = .false.
-        end if
-    end function originatingModuleIsS
+        originatedFromModule = self%tail%entry_.isFrom.module_
+    end function originatedFromModule
 
-    pure function originatingProcedureIsC(self, procedure_name)
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: procedure_name
-        logical :: originatingProcedureIsC
-
-        if (allocated(self%entries)) then
-            originatingProcedureIsC = &
-                    self%entries(size(self%entries)).isFromProcedure.procedure_name
-        else
-            originatingProcedureIsC = .false.
-        end if
-    end function originatingProcedureIsC
-
-    pure function originatingProcedureIsS(self, procedure_name)
-        use iso_varying_string, only: VARYING_STRING
+    pure function originatedFromProcedure(self, procedure_)
+        use Procedure_m, only: Procedure_t
 
         class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: procedure_name
-        logical :: originatingProcedureIsS
+        type(Procedure_t), intent(in) :: procedure_
+        logical :: originatedFromProcedure
 
-        if (allocated(self%entries)) then
-            originatingProcedureIsS = &
-                    self%entries(size(self%entries)).isFromProcedure.procedure_name
-        else
-            originatingProcedureIsS = .false.
-        end if
-    end function originatingProcedureIsS
+        originatedFromProcedure = self%tail%entry_.isFrom.procedure_
+    end function originatedFromProcedure
 
-    pure function containsModuleC(self, module_name)
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: module_name
-        logical :: containsModuleC
-
-        if (allocated(self%entries)) then
-            containsModuleC = any(self%entries.isFromModule.module_name)
-        else
-            containsModuleC = .false.
-        end if
-    end function containsModuleC
-
-    pure function containsModuleS(self, module_name)
-        use iso_varying_string, only: VARYING_STRING
+    function includesModule(self, module_)
+        use Module_m, only: Module_t
 
         class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: module_name
-        logical :: containsModuleS
+        type(Module_t), intent(in) :: module_
+        logical :: includesModule
 
-        if (allocated(self%entries)) then
-            containsModuleS = any(self%entries.isFromModule.module_name)
-        else
-            containsModuleS = .false.
-        end if
-    end function containsModuleS
+        type(Node_t), pointer :: current
 
-    pure function containsProcedureC(self, procedure_name)
-        class(CallStack_t), intent(in) :: self
-        character(len=*), intent(in) :: procedure_name
-        logical :: containsProcedureC
+        includesModule = .false.
+        current => self%head
+        do while (associated(current))
+            if (current%entry_.isFrom.module_) then
+                includesModule = .true.
+                return
+            end if
+            current => current%next
+        end do
+    end function includesModule
 
-        if (allocated(self%entries)) then
-            containsProcedureC = any(self%entries.isFromProcedure.procedure_name)
-        else
-            containsProcedureC = .false.
-        end if
-    end function containsProcedureC
-
-    pure function containsProcedureS(self, procedure_name)
-        use iso_varying_string, only: VARYING_STRING
+    function includesProcedure(self, procedure_)
+        use Procedure_m, only: Procedure_t
 
         class(CallStack_t), intent(in) :: self
-        type(VARYING_STRING), intent(in) :: procedure_name
-        logical :: containsProcedureS
+        type(Procedure_t), intent(in) :: procedure_
+        logical :: includesProcedure
 
-        if (allocated(self%entries)) then
-            containsProcedureS = any(self%entries.isFromProcedure.procedure_name)
-        else
-            containsProcedureS = .false.
-        end if
-    end function containsProcedureS
+        type(Node_t), pointer :: current
 
-    pure function repr(self)
-        use iso_varying_string, only: &
-                VARYING_STRING, assignment(=), operator(//)
+        includesProcedure = .false.
+        current => self%head
+        do while (associated(current))
+            if (current%entry_.isFrom.procedure_) then
+                includesProcedure = .true.
+                return
+            end if
+            current => current%next
+        end do
+    end function includesProcedure
+
+    function repr(self)
+        use iso_varying_string, only: VARYING_STRING, operator(//)
         use strff, only: hangingIndent, join, NEWLINE
 
         class(CallStack_t), intent(in) :: self
         type(VARYING_STRING) :: repr
 
-        if (allocated(self%entries)) then
-            repr = hangingIndent( &
-                    "CallStack([" // NEWLINE &
-                    // join(self%entries%repr(), "," // NEWLINE) &
-                    // "])", &
-                    4)
-        else
-            repr = "CallStack([])"
-        end if
+        type(Node_t), pointer :: current
+        type(VARYING_STRING) :: entry_strings(self%depth)
+        integer :: i
+
+        current => self%head
+        do i = 1, self%depth
+            entry_strings(i) = current%entry_%repr()
+            current => current%next
+        end do
+        repr = hangingIndent( &
+                "CallStack([" // NEWLINE &
+                        // join(entry_strings, "," // NEWLINE) // "])", &
+                4)
     end function repr
+
+    subroutine destructor(self)
+        type(CallStack_t), intent(inout) :: self
+
+        type(Node_t), pointer :: current
+        type(Node_t), pointer :: next
+
+        current => self%head
+        do while (associated(current))
+            next => current%next
+            deallocate(current)
+            current => next
+        end do
+    end subroutine destructor
 end module Call_stack_m
