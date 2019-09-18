e82818b7ef5597bfc40db3c9b291d1c40d29ed02
Brad Richardson <brichardson@structint.com>
2019-09-18 14:21:16 -0700

    Append one list to another

--- src/Message_list_m.f90
+++ src/Message_list_m.f90
@@ -12,10 +12,13 @@ module Message_list_m
     type, public :: MessageList_t
         private
         type(Node_t), pointer :: head => null()
+        type(Node_t), pointer :: tail => null()
     contains
         private
         procedure, public :: appendMessage
+        procedure, public :: appendMessages
         procedure, public :: toString
+        final :: destructor
     end type MessageList_t
 contains
     subroutine appendMessage(self, message)
@@ -24,21 +27,64 @@ contains
         class(MessageList_t), intent(inout) :: self
         class(Message_t), pointer, intent(in) :: message
 
-        allocate(self%head)
-        self%head%message => message
-        nullify(self%head%next)
+        if (associated(self%head)) then
+            allocate(self%tail%next)
+            self%tail => self%tail%next
+            self%tail%message => message
+            nullify(self%tail%next)
+        else
+            allocate(self%head)
+            self%head%message => message
+            nullify(self%head%next)
+            self%tail => self%head
+        end if
     end subroutine appendMessage
 
+    subroutine appendMessages(self, messages)
+        class(MessageList_t), intent(inout) :: self
+        type(MessageList_t), intent(inout) :: messages
+
+        allocate(self%head)
+        self%head => messages%head
+        nullify(messages%head)
+    end subroutine appendMessages
+
     function toString(self) result(string)
-        use iso_varying_string, only: VARYING_STRING, assignment(=)
+        use iso_varying_string, only: VARYING_STRING
 
         class(MessageList_t), intent(in) :: self
         type(VARYING_STRING) :: string
 
-        if (associated(self%head)) then
-            string = self%head%message%toString()
-        else
-            string = ""
-        end if
+        string = recursiveToString(self%head)
+    contains
+        recursive function recursiveToString(node) result(string_)
+            use iso_varying_string, only: &
+                    VARYING_STRING, assignment(=), operator(//)
+            use strff, only: NEWLINE
+
+            type(Node_t), pointer :: node
+            type(VARYING_STRING) :: string_
+
+            if (associated(node)) then
+                string_ = &
+                        node%message%toString() &
+                        // NEWLINE &
+                        // recursiveToString(node%next)
+            else
+                string_ = ""
+            end if
+        end function recursiveToString
     end function toString
+
+    subroutine destructor(self)
+        type(MessageList_t), intent(inout) :: self
+
+        type(Node_t), pointer :: current
+
+        current => self%head
+        do while (associated(current))
+            deallocate(current%message)
+            current => current%next
+        end do
+    end subroutine destructor
 end module Message_list_m
--- tests/message_list_test.f90
+++ tests/message_list_test.f90
@@ -9,16 +9,16 @@ contains
 
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(2)
+        type(TestItem_t) :: individual_tests(3)
 
         individual_tests(1) = it( &
                 "converts to an empty string when it is empty", &
                 checkEmptyToString)
         individual_tests(2) = it( &
                 "can append to an empty list", checkAppendToEmpty)
-        ! individual_tests(3) = it( &
-        !         "can append multiple to an empty list", &
-        !         checkAppendMultipleToEmpty)
+        individual_tests(3) = it( &
+                "can append multiple to an empty list", &
+                checkAppendMultipleToEmpty)
         ! individual_tests(4) = it( &
         !         "can append an empty list", checkAppendEmpty)
         ! individual_tests(5) = it( &
@@ -69,30 +69,32 @@ contains
 
         result_ = assertIncludes(MESSAGE, message_list%toString())
     end function checkAppendToEmpty
-    !
-    ! pure function checkAppendMultipleToEmpty() result(result_)
-    !     use Message_m, only: Info, Warning
-    !     use Message_list_m, only: MessageList_t
-    !     use Vegetables_m, only: Result_t, assertIncludes
-    !
-    !     type(Result_t) :: result_
-    !
-    !     character(len=*), parameter :: FIRST_MESSAGE = "First Message"
-    !     character(len=*), parameter :: SECOND_MESSAGE = "Second Message"
-    !     type(MessageList_t) :: message_list1
-    !     type(MessageList_t) :: message_list2
-    !
-    !     message_list2 = message_list2%appendMessage(Info( &
-    !             "Some_module_m", "someProcedure", FIRST_MESSAGE))
-    !     message_list2 = message_list2%appendMessage(Warning( &
-    !             "Some_module_m", "someProcedure", SECOND_MESSAGE))
-    !
-    !     message_list1 = message_list1%appendMessages(message_list2)
-    !
-    !     result_ = &
-    !             assertIncludes(FIRST_MESSAGE, message_list1%toString()) &
-    !             .and.assertIncludes(SECOND_MESSAGE, message_list1%toString())
-    ! end function checkAppendMultipleToEmpty
+
+    function checkAppendMultipleToEmpty() result(result_)
+        use Message_m, only: Info, Warning
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        character(len=*), parameter :: FIRST_MESSAGE = "First Message"
+        character(len=*), parameter :: SECOND_MESSAGE = "Second Message"
+        type(MessageList_t) :: message_list1
+        type(MessageList_t) :: message_list2
+
+        call message_list2%appendMessage(Info( &
+                Module_("Some_m"), Procedure_("some"), FIRST_MESSAGE))
+        call message_list2%appendMessage(Warning( &
+                Module_("Some_m"), Procedure_("some"), SECOND_MESSAGE))
+
+        call message_list1%appendMessages(message_list2)
+
+        result_ = &
+                assertIncludes(FIRST_MESSAGE, message_list1%toString()) &
+                .and.assertIncludes(SECOND_MESSAGE, message_list1%toString())
+    end function checkAppendMultipleToEmpty
     !
     ! pure function checkAppendEmpty() result(result_)
     !     use Message_m, only: Info, Warning
