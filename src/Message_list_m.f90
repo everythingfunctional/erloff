089f89bc664726aacc4b24a317b5f5ffa2d6db47
Brad Richardson <brichardson@structint.com>
2019-10-15 13:30:21 -0700

    Implement appendMessage

--- src/Message_list_m.f90
+++ src/Message_list_m.f90
@@ -1,22 +1,37 @@
 module Message_list_m
+    use Message_m, only: Message_t
+
     implicit none
     private
 
     type, public :: MessageList_t
+        private
+        class(Message_t), allocatable :: message
     contains
         private
+        procedure, public :: appendMessage
         procedure, public :: toString
     end type MessageList_t
 contains
+    subroutine appendMessage(self, message)
+        use Message_m, only: Message_t
+
+        class(MessageList_t), intent(inout) :: self
+        class(Message_t), intent(in) :: message
+
+        allocate(self%message, source = message)
+    end subroutine appendMessage
+
     function toString(self) result(string)
         use iso_varying_string, only: VARYING_STRING, assignment(=)
 
         class(MessageList_t), intent(in) :: self
         type(VARYING_STRING) :: string
 
-        associate(a => self)
-        end associate
-
-        string = ""
+        if (allocated(self%message)) then
+            string = self%message%toString()
+        else
+            string = ""
+        end if
     end function toString
 end module Message_list_m
--- tests/message_list_test.f90
+++ tests/message_list_test.f90
@@ -9,11 +9,14 @@ contains
 
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(1)
+        type(TestItem_t) :: individual_tests(2)
 
         individual_tests(1) = It( &
                 "Converts to an empty string when it is empty", &
                 checkEmptyToString)
+        individual_tests(2) = It( &
+                "Can append a message to an empty list", &
+                checkAppendToEmpty)
         tests = Describe("MessageList_t", individual_tests)
     end function test_message_list
 
@@ -27,4 +30,23 @@ contains
 
         result_ = assertEmpty(message_list%toString())
     end function checkEmptyToString
+
+    function checkAppendToEmpty() result(result_)
+        use Message_m, only: Message_t, Info
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        class(Message_t), allocatable :: message
+        type(MessageList_t) :: message_list
+
+        allocate(message, source = Info( &
+                Module_("Some_m"), Procedure_("some"), "Test Message"))
+        call message_list%appendMessage(message)
+
+        result_ = assertIncludes(message%toString(), message_list%toString())
+    end function checkAppendToEmpty
 end module message_list_test
