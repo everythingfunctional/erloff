b137b1f1e2e7ff2b8ba801a53d6cd4b74746e74f
Brad Richardson <brichardson@structint.com>
2019-10-15 14:27:09 -0700

    Implement appendMessages

--- src/Message_list_m.f90
+++ src/Message_list_m.f90
@@ -4,12 +4,17 @@ module Message_list_m
     implicit none
     private
 
+    type :: MessageItem_t
+        class(Message_t), allocatable :: message
+    end type MessageItem_t
+
     type, public :: MessageList_t
         private
-        class(Message_t), allocatable :: message
+        type(MessageItem_t), allocatable :: messages(:)
     contains
         private
         procedure, public :: appendMessage
+        procedure, public :: appendMessages
         procedure, public :: toString
     end type MessageList_t
 contains
@@ -19,17 +24,72 @@ contains
         class(MessageList_t), intent(inout) :: self
         class(Message_t), intent(in) :: message
 
-        allocate(self%message, source = message)
+        type(MessageItem_t) :: old_messages(size(self%messages))
+
+        if (allocated(self%messages)) then
+            old_messages = self%messages
+            deallocate(self%messages)
+            allocate(self%messages(size(old_messages) + 1))
+            self%messages(1:size(old_messages)) = old_messages
+            allocate(self%messages(size(self%messages))%message, source = message)
+        else
+            allocate(self%messages(1))
+            allocate(self%messages(1)%message, source = message)
+        end if
     end subroutine appendMessage
+
+    subroutine appendMessages(self, messages, module_, procedure_)
+        use Module_m, only: Module_t
+        use Procedure_m, only: Procedure_t
+
+        class(MessageList_t), intent(inout) :: self
+        type(MessageList_t), intent(in) :: messages
+        type(Module_t), intent(in) :: module_
+        type(Procedure_t), intent(in) :: procedure_
+
+        integer :: i
+        type(MessageItem_t) :: old_messages(size(self%messages))
+        integer :: num_new_messages
+        integer :: num_old_messages
+        integer :: total_num_messages
+
+        if (allocated(messages%messages)) then
+            if (allocated(self%messages)) then
+                num_old_messages = size(self%messages)
+                num_new_messages = size(messages%messages)
+                total_num_messages = num_old_messages + num_new_messages
+                old_messages = self%messages
+                deallocate(self%messages)
+                allocate(self%messages(total_num_messages))
+                self%messages(1:num_old_messages) = old_messages
+                self%messages(num_old_messages+1:) = messages%messages
+                do i = num_old_messages + 1, total_num_messages
+                    call self%messages(i)%message%prependNames(module_, procedure_)
+                end do
+            else
+                allocate(self%messages, source = messages%messages)
+                do i = 1, size(self%messages)
+                    call self%messages(i)%message%prependNames(module_, procedure_)
+                end do
+            end if
+        end if
+    end subroutine appendMessages
 
     function toString(self) result(string)
         use iso_varying_string, only: VARYING_STRING, assignment(=)
+        use strff, only: join, NEWLINE
 
         class(MessageList_t), intent(in) :: self
         type(VARYING_STRING) :: string
 
-        if (allocated(self%message)) then
-            string = self%message%toString()
+        integer :: i
+        type(VARYING_STRING) :: strings(size(self%messages))
+
+        if (allocated(self%messages)) then
+            do i = 1, size(self%messages)
+                strings(i) = self%messages(i)%message%toString()
+            end do
+            string = join(strings, NEWLINE)
         else
             string = ""
         end if
--- tests/message_list_test.f90
+++ tests/message_list_test.f90
@@ -9,7 +9,7 @@ contains
 
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(2)
+        type(TestItem_t) :: individual_tests(6)
 
         individual_tests(1) = It( &
                 "Converts to an empty string when it is empty", &
@@ -17,6 +17,15 @@ contains
         individual_tests(2) = It( &
                 "Can append a message to an empty list", &
                 checkAppendToEmpty)
+        individual_tests(3) = It( &
+                "Can append multiple to an empty list", &
+                checkAppendMultipleToEmpty)
+        individual_tests(4) = It( &
+                "Can append an empty list", checkAppendEmpty)
+        individual_tests(5) = It( &
+                "Can combine two empty lists", checkCombineEmpty)
+        individual_tests(6) = It( &
+                "Can combine two lists", checkCombine)
         tests = Describe("MessageList_t", individual_tests)
     end function test_message_list
 
@@ -49,4 +58,119 @@ contains
 
         result_ = assertIncludes(message%toString(), message_list%toString())
     end function checkAppendToEmpty
+
+    function checkAppendMultipleToEmpty() result(result_)
+        use Message_m, only: Message_t, Info, Warning
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        class(Message_t), allocatable :: message1
+        class(Message_t), allocatable :: message2
+        type(MessageList_t) :: message_list1
+        type(MessageList_t) :: message_list2
+
+        allocate(message1, source = Info( &
+                Module_("Some_m"), Procedure_("some"), "First Message"))
+        allocate(message2, source = Warning( &
+                Module_("Some_m"), Procedure_("some"), "Second Message"))
+        call message_list1%appendMessage(message1)
+        call message_list1%appendMessage(message2)
+
+        call message_list2%appendMessages( &
+                message_list1, Module_("Another_m"), Procedure_("another"))
+
+        result_ = &
+                assertIncludes(message1%toString(), message_list2%toString()) &
+                .and.assertIncludes(message2%toString(), message_list2%toString())
+    end function checkAppendMultipleToEmpty
+
+    function checkAppendEmpty() result(result_)
+        use Message_m, only: Message_t, Info, Warning
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        class(Message_t), allocatable :: message1
+        class(Message_t), allocatable :: message2
+        type(MessageList_t) :: message_list1
+        type(MessageList_t) :: message_list2
+
+        allocate(message1, source = Info( &
+                Module_("Some_m"), Procedure_("some"), "First Message"))
+        allocate(message2, source = Warning( &
+                Module_("Some_m"), Procedure_("some"), "Second Message"))
+        call message_list1%appendMessage(message1)
+        call message_list1%appendMessage(message2)
+
+        call message_list1%appendMessages( &
+                message_list2, Module_("Another_m"), Procedure_("another"))
+
+        result_ = &
+                assertIncludes(message1%toString(), message_list1%toString()) &
+                .and.assertIncludes(message2%toString(), message_list1%toString())
+    end function checkAppendEmpty
+
+    function checkCombineEmpty() result(result_)
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertEmpty
+
+        type(Result_t) :: result_
+
+        type(MessageList_t) :: message_list1
+        type(MessageList_t) :: message_list2
+
+        call message_list1%appendMessages( &
+                message_list2, Module_("Another_m"), Procedure_("another"))
+
+        result_ = assertEmpty(message_list1%toString())
+    end function checkCombineEmpty
+
+    function checkCombine() result(result_)
+        use Message_m, only: Message_t, Info, Warning
+        use Message_list_m, only: MessageList_t
+        use Module_m, only: Module_
+        use Procedure_m, only: Procedure_
+        use Vegetables_m, only: Result_t, assertIncludes
+
+        type(Result_t) :: result_
+
+        class(Message_t), allocatable :: message1
+        class(Message_t), allocatable :: message2
+        class(Message_t), allocatable :: message3
+        class(Message_t), allocatable :: message4
+        type(MessageList_t) :: message_list1
+        type(MessageList_t) :: message_list2
+
+        allocate(message1, source = Info( &
+                Module_("Some_m"), Procedure_("some"), "First Message"))
+        allocate(message2, source = Warning( &
+                Module_("Some_m"), Procedure_("some"), "Second Message"))
+        call message_list1%appendMessage(message1)
+        call message_list1%appendMessage(message2)
+
+        allocate(message3, source = Info( &
+                Module_("Some_m"), Procedure_("some"), "Third Message"))
+        allocate(message4, source = Warning( &
+                Module_("Some_m"), Procedure_("some"), "Fourth Message"))
+        call message_list2%appendMessage(message3)
+        call message_list2%appendMessage(message4)
+
+        call message_list1%appendMessages( &
+                message_list2, Module_("Another_m"), Procedure_("another"))
+
+        result_ = &
+                assertIncludes(message1%toString(), message_list1%toString()) &
+                .and.assertIncludes(message2%toString(), message_list1%toString()) &
+                .and.assertIncludes(message3%toString(), message_list1%toString()) &
+                .and.assertIncludes(message4%toString(), message_list1%toString())
+    end function checkCombine
 end module message_list_test
